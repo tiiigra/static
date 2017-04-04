82b4d7abfc03541716b995d61c090a380cb20284
Tatiana Baklan <baklantat@gmail.com>
2017-04-04 14:21:50 +0800

    Basic setup

---
+++ server/index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import path from 'path';
+
+let app = express();
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+app.listen(3000, () => {
+    console.log('Running on port 3000');
+});
