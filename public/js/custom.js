69cc29ca7ca33200a57c9e84ca918bfe14901d13
Tatiana Baklan <baklantat@gmail.com>
2017-04-26 21:58:48 +0800

    Add public files

---
+++ public/js/custom.js
@@ -0,0 +1,23 @@
+// preloader
+$(window).load(function(){
+    $('.preloader').fadeOut(1000); // set duration in brackets    
+});
+
+$(function() {
+    new WOW().init();
+    $('.templatemo-nav').singlePageNav({
+    	offset: 70
+    });
+
+    $('.templatemo-box').singlePageNav({
+        offset: 70
+    });
+
+
+
+    /* Hide mobile menu after clicking on a link
+    -----------------------------------------------*/
+    $('.navbar-collapse a').click(function(){
+        $(".navbar-collapse").collapse('hide');
+    });
+})
