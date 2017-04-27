ec3dd62532f800f92dd48b7abb625d9fc5226706
Tatiana Baklan <baklantat@gmail.com>
2017-04-27 14:39:58 +0800

    Changed 8080 to port variable

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express =  require("express");
 var path = require("path");
 
 var  app = express();
-//var port = process.env.PORT || 8080;
+var port = process.env.PORT || 8080;
 
 
 app.use(express.static(path.join(__dirname, './public/')));
@@ -11,6 +11,6 @@ app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname,  './public/index.html'));
 });
 
-app.listen(8080, function()  {
+app.listen(port, function()  {
     console.log('Running on port 8080');
 });
