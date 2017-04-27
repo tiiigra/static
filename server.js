57e4052b1e4e95ee7be98f349591d397b3719757
Tatiana Baklan <baklantat@gmail.com>
2017-04-27 14:24:39 +0800

    Change ES6 to ES5

---
+++ server.js
@@ -0,0 +1,16 @@
+var express =  require("express");
+var path = require("path");
+
+var  app = express();
+//var port = process.env.PORT || 8080;
+
+
+app.use(express.static(path.join(__dirname, './public/')));
+
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname,  './public/index.html'));
+});
+
+app.listen(8080, function()  {
+    console.log('Running on port 8080');
+});
